0db7e86b882cda9a7dfeeaaff66815e3b38ef9cd
Jamie Holloway <jamie@cadiahq.com>
2022-10-21 15:37:14 +0100

    oy vey

--- web3/pages/_app.js
+++ web3/pages/_app.js
@@ -1,6 +1,22 @@
 import '../styles/globals.css'
+import {useEffect} from 'react'
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    addKittyHandler();
+  })
+
+  async function addKittyHandler(){
+        const response = await fetch('/api/getKitty', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const result = await response.json();
+        console.log(result)
+    }
   return <Component {...pageProps} />
 }
 
